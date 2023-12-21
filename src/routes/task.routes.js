fd807c19cc4f01a4edfe2196ddd2400ef4c0d51d
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-21 11:58:37 -0300

    controllers added 2

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
-import { getAllTasks, getTask, createTask, updateTask, deleteTask } from "../controllers/tasks.controller.js";
+import {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/tasks.controller.js";
 
 const router = Router();
 
 router.get("/tasks", getAllTasks);
 
-router.get("/tasks/:id", getTask); 
+router.get("/tasks/:id", getTask);
 
-router.post("/tasks", createTask );
+router.post("/tasks", createTask);
 
 router.put("/tasks/:id", updateTask);
 
