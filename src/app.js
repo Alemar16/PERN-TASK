91989c39b4ae3c1d21119d150026ff7f9c126af4
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-21 11:30:28 -0300

    routes auth added

--- src/app.js
+++ src/app.js
@@ -1,20 +1,22 @@
 import express from "express";
 import morgan from "morgan";
+
 import taksRoutes from "./routes/task.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
+//middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//routes
+app.get("/", (req, res) => {res.json({ message: "Welcome to my API" });});
+app.use("/api", taksRoutes);
+app.use("/api", authRoutes);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to my API" });
-});
-
-app.use(taksRoutes);
-
+//error handler
 app.use((err, req, res, next) => {
   res.status(500).json({
     status: "error",
