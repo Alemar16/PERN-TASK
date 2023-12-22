eb4f86aef806bfa2c8834e08cd9ac896f300b20d
Armando Martinez <alemar.martinez16@gmail.com>
2023-12-22 08:25:23 -0300

    crear database como tasks_db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import app from "./app.js";
 
 app.listen(3000);
-
 console.log("Server on port 3000");
+
+/* import app from "./app.js";
+import {pool} from "./db.js";
+
+pool.query("SELECT NOW()", (err, res) => {
+    console.log(err, res);
+    app.listen(3000);
+    console.log("Server on port 3000");
+    pool.end();
+}) */
